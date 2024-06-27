022c9f5157e0d9eef916c08dfdc82244705bc5bf
fels123 <kelengulienakhro@gmail.com>
2024-06-27 14:57:35 +0530

    combining 2 arrays into one or splitting arrray

--- index.js
+++ index.js
@@ -79,3 +79,12 @@
 // console.log(another);
 
 // ************************************
+// combine 2 arrays or slice an array into 2 parts
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second); //combine this 2 array into one
+console.log(combined);
+
+const sliced = combined.slice(2);
+console.log(sliced);
