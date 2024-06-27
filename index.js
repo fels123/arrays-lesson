d0fc840f89654015e58925196812b872e7ad3a7a
fels123 <kelengulienakhro@gmail.com>
2024-06-27 15:03:27 +0530

    combinig 2 arrays using spread operator

--- index.js
+++ index.js
@@ -80,11 +80,19 @@
 
 // ************************************
 // combine 2 arrays or slice an array into 2 parts
+// const first = [1, 2, 3];
+// const second = [4, 5, 6];
+
+// const combined = first.concat(second); //combine this 2 array into one
+// console.log(combined);
+
+// const sliced = combined.slice(2);
+// console.log(sliced);
+
+// *********************************************
+//Spread Operator to achieve the same result of combining 2 arrays
 const first = [1, 2, 3];
 const second = [4, 5, 6];
 
-const combined = first.concat(second); //combine this 2 array into one
-console.log(combined);
-
-const sliced = combined.slice(2);
-console.log(sliced);
+const combined = [...first, ...second]; //same result as const combined = first.concat(second)
+const copy = [...combined]; // same const sliced = combined.slice(2);
