364b01499bb3afa655dba672d9ec8c48fa4dd586
fels123 <kelengulienakhro@gmail.com>
2024-06-27 13:51:06 +0530

    finding elements in an array in primitive types

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 //ADDING ELEMENTS IN AN ARRAY
 
-const numbers = [3, 4];
+// const numbers = [3, 4];
 
-// add array to end
-numbers.push(5, 6);
-console.log(numbers);
+// // add array to end
+// numbers.push(5, 6);
+// console.log(numbers);
 
-// add array to beginning
-numbers.unshift(7, 8);
-console.log(numbers);
+// // add array to beginning
+// numbers.unshift(7, 8);
+// console.log(numbers);
 
-// add array to middle
-numbers.splice(3, 0, "a", "b"); //add or remove
-console.log(numbers);
+// // add array to middle
+// numbers.splice(3, 0, "a", "b"); //add or remove
+// console.log(numbers);
+
+// ***************************************************************************
+// finding elements in ab array primitive type
+
+// const numbers = [1, 2, 3, 1, 1, 4];
+// console.log(numbers.indexOf(1));
+// console.log(numbers.indexOf(1, 0));
+
+// console.log(numbers.includes(1));
+
+// *******************************************************
