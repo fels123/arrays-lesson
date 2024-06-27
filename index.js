263d389f20fb9c38183d02f811e9ae48342af5e2
fels123 <kelengulienakhro@gmail.com>
2024-06-27 14:35:49 +0530

    finding elements in an array in a reference type array

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
 // console.log(numbers);
 
 // ***************************************************************************
-// finding elements in ab array primitive type
+// finding elements in an array primitive type
 
 // const numbers = [1, 2, 3, 1, 1, 4];
 // console.log(numbers.indexOf(1));
@@ -24,3 +24,20 @@
 // console.log(numbers.includes(1));
 
 // *******************************************************
+// finding elements in an array in  refrence type
+const courses = [
+  { id: Math.random(), name: "a" },
+  { id: Math.random(), name: "b" },
+];
+
+const found = courses.find(function (course) {
+  return course.name === "a";
+});
+console.log(found);
+//other method
+const course = courses.findIndex(function (course) {
+  return course.name === "a";
+});
+console.log(course);
+
+// **********************************************
